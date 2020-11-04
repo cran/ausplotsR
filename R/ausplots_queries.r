b6fe394e3dfbaaa3ec40aaebe4a547f254ecab41
Greg Guerin <ggueri01@gmail.com>
2020-11-04 14:10:02 +0000

    version 1.2

---
+++ R/ausplots_queries.r
@@ -0,0 +1,210 @@
+cache <- new.env(parent = emptyenv())
+
+.ausplots_api <- function(path, query) {
+  # override this API URL with:
+  #   library("ausplotsR")
+  #   options("ausplotsR_api_url" = "http://localhost:30000")
+  #   ...continue to call functions
+  auth_header <- ""
+  the_role <- getOption("ausplotsR_role")
+  the_secret <- getOption("ausplotsR_secret")
+  if (!is.null(the_role) && !is.null(the_secret)) {
+    path <- paste(path, "_inc_unpub", sep="")
+    five_days <- 60 * 60 * 24 * 5
+    exp_time <- as.integer(Sys.time() + five_days)
+    claim <- jose::jwt_claim(role = the_role, exp = exp_time)
+    jwt_val <- jose::jwt_encode_hmac(claim, secret = charToRaw(the_secret))
+    auth_header <- paste('Bearer', jwt_val)
+  }
+  if (getOption("ausplotsR_api_debug", default = FALSE)) {
+    message('query string value = ', query)
+  }
+  resp <- httr::GET(
+                    getOption("ausplotsR_api_url", default= "http://swarmapi.ausplots.aekos.org.au:80"),
+                    httr::user_agent(.make_user_agent()),
+                    httr::add_headers(Authorization = auth_header),
+                    path=path,
+                    query=query
+  )
+  httr::stop_for_status(resp, task = httr::content(resp, "text"))
+  if (httr::http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+  return(jsonlite::fromJSON(httr::content(resp, "text"), simplifyDataFrame = TRUE))
+}
+################
+#initial filter function
+.ausplots_api_with_plot_filter <- function(path, Plot_IDs_to_filter_for, extra_query=list()) {
+  query <- extra_query
+  if (length(Plot_IDs_to_filter_for) > 0) {
+    plotFilter <- paste("in.(", paste(Plot_IDs_to_filter_for, collapse=","), ")", sep="")
+    query <- c(query, list(site_location_name = plotFilter))
+  }
+  return(.ausplots_api(path, query)) 
+} 
+################
+#subsequent filter function
+
+.ausplots_api_with_specified_plot_ids <- function(path, Plot_IDs_to_retrieve_data_for, extra_query=list()) {
+  plotFilter <- paste("in.(", paste(Plot_IDs_to_retrieve_data_for, collapse=","), ")", sep="")
+  query <- c(extra_query, list(site_location_name = plotFilter))
+  return(.ausplots_api(path, query))
+} 
+################
+
+list_available_plots <- function(Plot_IDs=c(), bounding_box="none", herbarium_determination_search=NULL, family_search=NULL, standardised_name_search=NULL) {
+  extra_query <- list()
+  
+  if(!is.null(family_search)) {
+    extra_query = append(extra_query, list("family" = paste("ilike.*", family_search, "*", sep=""))) #search by family
+  }
+  
+  if(!is.null(herbarium_determination_search)) {
+    extra_query = append(extra_query, list("herbarium_determination" = paste("ilike.*", herbarium_determination_search, "*", sep=""))) #search by herbarium_determination
+  } 
+  if(!is.null(standardised_name_search)) {
+    extra_query = append(extra_query, list("standardised_name" = paste("ilike.*", standardised_name_search, "*", sep=""))) #search by standardised_name
+  }
+  if(Plot_IDs[1] == "none") {
+    Plot_IDs <- c()
+  }
+  if(bounding_box[1] != "none") { #i.e. if user has supplied an extent vector
+    if(class(bounding_box) != "numeric" | length(bounding_box) != 4) {stop("Bounding box must be a numeric vector of length 4.")}
+    
+    min_lon <- bounding_box[1]
+    max_lon <- bounding_box[2]
+    min_lat <- bounding_box[3]
+    max_lat <- bounding_box[4]
+    
+    extra_query = append(extra_query, list("latitude" = paste("gte.", min_lat, sep="")))
+    extra_query = append(extra_query, list("latitude" = paste("lte.", max_lat, sep="")))
+    extra_query = append(extra_query, list("longitude" = paste("gte.", min_lon, sep="")))
+    extra_query = append(extra_query, list("longitude" = paste("lte.", max_lon, sep="")))
+  }
+
+  path <- "search"
+  response <- .ausplots_api_with_plot_filter(path, Plot_IDs, extra_query)
+  result <- unique(response$site_location_name)
+  return(result)
+}
+
+################
+
+extract_site_info <- function(Plot_IDs) {
+  path <- "site"
+  result<-.ausplots_api_with_specified_plot_ids(path, Plot_IDs)
+  result$state = mapply(function(x) substr(x, 0, 2), result$site_location_name)
+  result$state[result$state == "NS"] <- "NSW"
+  result$state[result$state == "QD"] <- "QLD"
+  result$state[result$state == "TC"] <- "TAS"
+  result$state[result$state == "VC"] <- "VIC"
+  
+  return(result)
+} 
+
+###############
+
+extract_struct_summ <- function(Plot_IDs) {
+  path <- "structural_summary"
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs))
+} 
+
+####################
+
+extract_soil_subsites <- function(Plot_IDs) {
+  path <- "soil_subsite"
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs))
+}
+
+##################
+
+extract_bulk_density <- function(Plot_IDs) {
+  path <- "soil_bulk_density"
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs))
+}
+
+##################
+
+extract_soil_char <- function(Plot_IDs) {
+  path <- "soil_characterisation"
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs))
+}
+
+##################
+
+extract_basal <- function(Plot_IDs, herbarium_determination_search=NULL, family_search=NULL, standardised_name_search=NULL) {
+  extra_query <- list() 
+   path <- "veg_basal"
+   if(!is.null(family_search)) {
+     extra_query = append(extra_query, list("family" = paste("ilike.*", family_search, "*", sep="")))#search by family 
+   }
+   if(!is.null(herbarium_determination_search)) {
+     extra_query = append(extra_query, list("herbarium_determination" = paste("ilike.*", herbarium_determination_search, "*", sep=""))) #search by herbarium_determination
+   } 
+   if(!is.null(standardised_name_search)) { 
+     extra_query = append(extra_query, list("standardised_name" = paste("ilike.*", standardised_name_search, "*", sep="")))#search by standardised_name
+   } 
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs, extra_query))
+}
+
+############################
+
+extract_vouch <- function(Plot_IDs, herbarium_determination_search=NULL, family_search=NULL, standardised_name_search=NULL) {
+  extra_query <- list()
+  path <- "veg_voucher"
+  if(!is.null(family_search)) {
+    extra_query = append(extra_query, list("family" = paste("ilike.*", family_search, "*", sep="")))#search by family 
+  }
+  if(!is.null(herbarium_determination_search)) {
+    extra_query = append(extra_query, list("herbarium_determination" = paste("ilike.*", herbarium_determination_search, "*", sep=""))) #search by herbarium_determination
+  } 
+  if(!is.null(standardised_name_search)) { 
+    extra_query = append(extra_query, list("standardised_name" = paste("ilike.*", standardised_name_search, "*", sep="")))#search by standardised_name
+  } 
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs, extra_query))
+}
+
+############################
+
+extract_hits <- function(Plot_IDs) {
+  path <- "veg_pi"
+  return(.ausplots_api_with_specified_plot_ids(path, Plot_IDs))
+}
+
+############################
+
+cache$user_agent <- NULL
+
+.make_user_agent <- function() {
+  # thanks https://github.com/r-lib/httr/blob/af25ebd0e3b72d2dc6e1423242b94efc25bc97cc/R/config.r#L137
+  if (is.null(cache$user_agent)) {
+    if (getOption("ausplotsR_api_debug", default = FALSE)) {
+      message('building user_agent string and storing in cache')
+    }
+    versions <- c(
+                  ausplotsR = as.character(utils::packageVersion("ausplotsR")),
+                  libcurl = curl::curl_version()$version,
+                  `r-curl` = as.character(utils::packageVersion("curl")),
+                  httr = as.character(utils::packageVersion("httr"))
+    )
+    result <- paste0(names(versions), "/", versions, collapse = " ")
+    cache$user_agent <- result
+  }
+  cache$user_agent
+}
+
+############################
+
+cache$metadata_dictionary <- NULL
+
+.get_metadata_dictionary <- function(force_refresh = FALSE) {
+  if (is.null(cache$metadata_dictionary) || force_refresh) {
+    if (getOption("ausplotsR_api_debug", default = FALSE)) {
+      message('refreshing metadata_dictionary and storing in cache')
+    }
+    path <- "metadata-dictionary.json"
+    result <- .ausplots_api(path, NULL)
+    cache$metadata_dictionary <- result
+  }
+  cache$metadata_dictionary
+}
---
+++ R/basal_area.R
@@ -0,0 +1,116 @@
+basal_area <- function(veg.basal, by.spp=FALSE, by.hits=FALSE, species_name=c("SN","HD","GS")) {
+	
+  
+  
+  basal <- veg.basal #for historical reasons, the compiled but raw data (referred to as simply 'basal' in the code)
+	
+	if(!by.hits) {
+		
+		if(!by.spp) {
+			
+			bas_areas <- plyr::count(basal, c("site_unique", "point_id"), wt_var="basal_area") #basal area scores for unique combos of site and point ID - i.e. for each of the 9 sample points in each plot, what is the total BA?
+			
+			bas_areas_mean <- stats::aggregate(bas_areas$freq, by=list(bas_areas$site_unique), FUN=mean) #mean of the basal areas for each of 9 sampling points in the plot. This is to give one value for each sampled plot, averaged across the 9 different sampling points and all species considered together.
+			
+			names(bas_areas_mean) <- c("site_unique", "basal_area_m2_ha")
+			
+			return(bas_areas_mean)
+
+		} #end if(!by.spp)
+		
+		if(by.spp) { #Basal Area by species per plot, default is standardised_name
+		
+		  if(missing(species_name)) { #if no species_name supplied, default to SN
+	       species_name = "SN"
+	       warning("No species_name supplied, defaulting to species_name='SN'")
+		  } #end missing
+		  
+		  
+		  if(species_name=="SN") { #uses standardised name
+		    
+		    bas_areas_spp_mean <- stats::aggregate(basal$basal_area, by=list(basal$site_unique, basal$standardised_name), FUN=mean)
+		    
+		    names(bas_areas_spp_mean) <- c("site_unique", "standardised_name", "basal_area_m2_ha")
+		  } #end SN
+		  
+		  if(species_name=="HD") { #uses herbarium determination
+		    
+		    bas_areas_spp_mean <- stats::aggregate(basal$basal_area, by=list(basal$site_unique, basal$herbarium_determination), FUN=mean)
+		    
+		    names(bas_areas_spp_mean) <- c("site_unique", "herbarium_determination", "basal_area_m2_ha") 
+		  } #end HD
+		    
+		    if(species_name=="GS") { #uses genus_species
+		      
+		      bas_areas_spp_mean <- stats::aggregate(basal$basal_area, by=list(basal$site_unique, basal$genus_species), FUN=mean)
+		      
+		      names(bas_areas_spp_mean) <- c("site_unique", "genus_species", "basal_area_m2_ha") 
+		    } #end GS
+		    
+		    return(bas_areas_spp_mean)
+		    
+		  }#end if(by.spp)
+	  
+	} #end if(!by.hits)
+	
+	
+	if(by.hits) { #Number of (tree) hits in the basal wedge sweep reps, giving a score related to tree density (there is no specific area as hits not confined to trees within the plot)
+		
+		if(!by.spp) {
+			
+			dens <- plyr::count(basal, c("site_unique", "point_id"), wt_var="hits") #number of hits for unique combos of site and point ID - i.e. for each of the 9 basal wedge sample points in each plot, what is the total number of scored trees for any spp recorded?
+			
+			dens_mean <- stats::aggregate(dens$freq, by=list(dens$site_unique), FUN=mean) #mean of the scores for each of the 9 reps - this is the average number of total tree hits for a rep at a given plot.
+			
+			names(dens_mean) <- c("site_unique", "mean_hits")
+			
+			return(dens_mean)
+			
+		} #end if(!by.spp)
+		
+	  if(by.spp) { #Hits by species:
+	    
+	    if(missing(species_name)) {
+	      species_name = "SN"
+	      warning("No species_name supplied, defaulting to species_name='SN'")
+	    }
+	    
+	    if(species_name=="SN") {
+	      
+	      dens_spp_mean <- stats::aggregate(basal$hits, by=list(basal$site_unique, basal$standardised_name), FUN=mean)
+	      
+	      names(dens_spp_mean) <- c("site_unique", "standardised_name", "mean_hits")
+	    } #end SN
+	    
+	      if(species_name=="HD") {
+	        
+	      ens_spp_mean <- stats::aggregate(basal$hits, by=list(basal$site_unique, basal$herbarium_determination), FUN=mean)
+	      
+	      names(dens_spp_mean) <- c("site_unique", "herbarium_determination", "mean_hits")
+	      
+	      }
+	      
+	      
+	      if(species_name=="GS") {
+	        
+	        basal<-basal[!is.na(basal$genus_species), ] #assumes that if a genus_species identification is not possible, the cell value for the row will be NA
+	        
+	        #alternatively, we might assign it a no data term, in which case
+	        #basal<-basal[!(basal$genus_species=="No ID",]
+	        
+	        ens_spp_mean <- stats::aggregate(basal$hits, by=list(basal$site_unique, basal$genus_species), FUN=mean)
+	        
+	        names(dens_spp_mean) <- c("site_unique", "genus_species", "mean_hits")
+	        
+	      }
+	      
+	      return(dens_spp_mean)
+	      
+	    
+	  } #end if(by.spp)
+	  
+	  
+	} #end if(by.hits)
+
+	
+} #end function
